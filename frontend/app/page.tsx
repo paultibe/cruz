4102563b60d717060527715d923e68d81a48cf8d
Paul Tiberghien <paultiberghien1@gmail.com>
2024-10-05 23:09:58 -0700

    google maps API working

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
-    </div>
-  );
-}
